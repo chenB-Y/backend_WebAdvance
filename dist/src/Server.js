b0d5de0ccf056f05953907cf3d0457af9fc745c1
chenB-Y <chenhaziza5941@gmail.com>
2024-08-01 11:07:28 +0300

    final

--- dist/src/Server.js
+++ dist/src/Server.js
@@ -14,16 +14,31 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const product_route_1 = __importDefault(require("./routes/product_route"));
 const auth_route_1 = __importDefault(require("./routes/auth_route"));
 const file_route_1 = __importDefault(require("./routes/file_route"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const group_route_1 = __importDefault(require("./routes/group_route"));
-const http_1 = __importDefault(require("http"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const events_1 = require("events");
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
 dotenv_1.default.config();
 const sseEmitter = new events_1.EventEmitter();
 sseEmitter.setMaxListeners(100);
 const app = (0, express_1.default)();
-const PORT = 4000;
-mongoose_1.default.connect(process.env.PROD_ENV = "production" ? process.env.DATABASE_URL_PROD : process.env.DATABASE_URL)
+const PORT = process.env.PORT || 4000;
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Web Dev 2024 REST API',
+            version: '1.0.0',
+            description: 'REST server including authentication using JWT',
+        },
+        servers: [{ url: 'https://node14.cs.colman.ac.il/' }],
+    },
+    apis: ['./src/routes/*.ts'],
+};
+const specs = (0, swagger_jsdoc_1.default)(swaggerOptions);
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(specs));
+mongoose_1.default.connect(process.env.PROD_ENV === 'production' ? process.env.DATABASE_URL_PROD : process.env.DATABASE_URL)
     .then(() => {
     console.log('Connected to DB');
 })
@@ -33,7 +48,6 @@ mongoose_1.default.connect(process.env.PROD_ENV = "production" ? process.env.DAT
 const allowedOrigins = ['https://10.10.248.174', 'https://node14.cs.colman.ac.il'];
 app.use((0, cors_1.default)({
     origin: (origin, callback) => {
-        // Check if the origin is in the allowed list
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
         }
@@ -44,10 +58,8 @@ app.use((0, cors_1.default)({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'refreshtoken'],
 }));
-// Body parser middleware
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
-// Routes
 app.use('/auth', auth_route_1.default);
 app.use('/product', product_route_1.default);
 app.use('/file', file_route_1.default);
@@ -56,37 +68,36 @@ app.use(express_1.default.static('/home/st111/projectCode/backend_WebAdvance/pub
 app.use(express_1.default.static('/home/st111/projectCode/backend_WebAdvance/public/users'));
 if (process.env.NODE_ENV !== 'production') {
     console.log('development');
-    http_1.default.createServer(app).listen(process.env.PORT);
-    console.log(`Server is running on PORT:${process.env.PORT}`);
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT: ${PORT}`);
+    });
 }
 else {
     console.log('production!');
-    // Load SSL certificates
     const keyPath = path_1.default.resolve(__dirname, '../../../../client-key.pem');
     const certPath = path_1.default.resolve(__dirname, '../../../../client-cert.pem');
     const options = {
         key: fs_1.default.readFileSync(keyPath),
         cert: fs_1.default.readFileSync(certPath),
     };
-    var hostname = "0.0.0.0";
-    var server = https_1.default.createServer(options, app);
-    server.listen(4000, hostname);
-    //  server.listen(4000,hostname, () => {
-    //  console.log(`Server is running on PORT:${process.env.PORT}`);
-    // });
-    app.get('/events', (req, res) => {
-        res.setHeader('Content-Type', 'text/event-stream');
-        res.setHeader('Cache-Control', 'no-cache');
-        res.setHeader('Connection', 'keep-alive');
-        const onData = (data) => {
-            res.write(`data: ${JSON.stringify(data)}\n\n`);
-        };
-        sseEmitter.on('event', onData);
-        req.on('close', () => {
-            sseEmitter.removeListener('event', onData);
-        });
+    const server = https_1.default.createServer(options, app);
+    const hostname = '0.0.0.0';
+    server.listen(4000, hostname, () => {
+        console.log(`Server is running on PORT: ${PORT}`);
     });
 }
+app.get('/events', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    const onData = (data) => {
+        res.write(`data: ${JSON.stringify(data)}\n\n`);
+    };
+    sseEmitter.on('event', onData);
+    req.on('close', () => {
+        sseEmitter.removeListener('event', onData);
+    });
+});
 function broadcast(data) {
     sseEmitter.emit('event', data);
 }
